9d0a305d0d0d5399410dabaf2e9e8db74efdfa83
Demircan Tas <tasd@mit.edu>
2023-11-22 23:10:48 -0500

    refactoring and modulization

---
+++ pr_augment.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+def adjust_gamma(image, gamma=1.0):
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    #  This is done using a 1D NumPy array (table)
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+                      for i in np.arange(0, 256)]).astype("uint8")
+
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
+
+def ad_th(image, p1 = 115, p2 = 1):
+    grayscaled = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    img_bgr = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, p1, p2)
+    return img_bgr
+
+def interactive_threshold(path):
+    # examples = ["sircali_buyuk_height1", "ince_kose_height"]
+    # path_strip = 'images/recognition/' + examples[1]
+    # path = path_strip + ".png"
+    # path_wr = path_strip + "_ADAPTIVE_THRESHOLD_" + ".png"
+    # path_wr = 'sourceimages/test_binary_ADAPTIVE_THRESHOLD.png'
+    path_wr = path[:-4] + '_ADAPTIVE_THRESHOLD.png'
+    cv2.namedWindow("frame")
+    cv2.createTrackbar("threshold param1", "frame", 115, 200, nothing)
+    cv2.createTrackbar("threshold param2", "frame", 1, 5, nothing)
+    cv2.createTrackbar("gamma", "frame", 100, 300, nothing)
+    cv2.createTrackbar("filter", "frame", 0, 100, nothing)
+
+    while True:
+        p1a = cv2.getTrackbarPos("threshold param1", "frame")
+        p1 = (int(p1a / 2)) * 2 + 3
+        p2 = cv2.getTrackbarPos("threshold param2", "frame")
+        gamma = float(cv2.getTrackbarPos("gamma", "frame")) / 100
+        filter = int(cv2.getTrackbarPos("filter", "frame") /2) * 2 +1
+
+        frame = cv2.imread(path)
+        frame = adjust_gamma(frame, gamma)
+        frame = ad_th(frame, p1, p2)
+
+        kernel = np.ones((filter,filter), np.uint8)
+        frame = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
+
+        # frame = cv2.GaussianBlur(frame, (15, 15), 0)
+        # frame = cv2.medianBlur(frame, filter)
+        # frame = cv2.bilateralFilter(frame, 15, 75, 75)
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame, ("p1 = " + str(p1a) + ", p2 = " + str(p2) + ", gamma = " +str(gamma)), (50, 50), font, 1, (0,0,255))
+
+        cv2.imshow('frame', frame)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+        elif key == 32:  # spacebar to save image
+            cv2.imwrite(path_wr, frame)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- pr_utility.py
+++ pr_utility.py
@@ -1,16 +1,16 @@
-from typing import Tuple
-
 import cv2
 import numpy as np
 import math
 import pickle
 
 def embedding(image):
+    '''
+    Takes an image, prompts user to draw a region of interest(ROI) 
+    and returns a tuple with integer coordinates of its corners
+    '''
     im = cv2.imread(image)
-    # Select Region of Interest
-    r = cv2.selectROI(im)
-
-    corners = (int(r[0]), int(r[1]), int(r[2]), int(r[3]))
+    rect = cv2.selectROI(im)
+    corners = (int(rect[0]), int(rect[1]), int(rect[2]), int(rect[3]))
 
     return corners
 
@@ -18,6 +18,9 @@ def nothing(x):
     pass
 
 def distance(point_a, point_b):
+    '''
+    Returns euclidean distance from tuple of two points
+    '''
     return math.sqrt((point_a[0] - point_b[0]) ** 2 + (point_a[1] - point_b[1]) ** 2)
 
 def corner_detect(image_path, embedding_coordinates, threshold):
@@ -31,14 +34,16 @@ def corner_detect(image_path, embedding_coordinates, threshold):
     def template_matching(img_bgr, image_gray, embedding):
         # w, h = embedding.shape[::-1]  # [::-1] is used to reverse the order of a list.
         w, h = embedding.shape  # new version. I don't understand why the list is reversed for w, h
-        # ndarray.shape function returns dimensions as a list(width, height, number of channels (in order)).
 
+        # ndarray.shape function returns dimensions as a list(width, height, number of channels (in order)).
         res = cv2.matchTemplate(image_gray, embedding, cv2.TM_CCOEFF_NORMED)  # returns an image
+
         # with match quality as pixel value. Inputs are the main image, embedding and method
         # methods are 'cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED' (used for thesis), 'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED'
         loc = np.where(res >= threshold)  # np.where returns two arrays containing coordinates of x, y
         points = zip(*loc[::-1])  # zip takes lists with equal numbers of elements, combines their members
         # "*" operator converts the operation to unzip
+
         corner_count = 0
         points_filtered = []
         mask = np.zeros(img_bgr.shape[:2], np.uint8)  # mask is used to filter out duplicate matches
@@ -46,14 +51,13 @@ def corner_detect(image_path, embedding_coordinates, threshold):
             if mask[pt[1] + int(h / 2), pt[0] + int(w / 2)] != 255:
                 mask[pt[1]:pt[1] + h, pt[0]:pt[0] + w] = 255
                 corner_count += 1
+
                 cv2.rectangle(img_bgr, pt, (pt[0] + w, pt[1] + h), (127, 127, 200), 2)
                 pt_centered = (int(pt[0] + w / 2), int(pt[1] + h / 2))
                 points_filtered.append(pt_centered)
-        # print(corner_count)
         return [img_bgr, points_filtered]
 
     return [template_matching(img_bgr, img_gray, embedding) for embedding in embeddings]
-
 
 def brick_lister(unsorted_corners):
     bricks = []  # list of lists with 4 items for brick corners
@@ -128,3 +132,85 @@ def brick_draw(image, brick):
         cv2.putText(image, str(area), (int(x) - 10, int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
         brickAttr = {"center_point": (int(x), int(y)), "area": area}
     return brickAttr
+
+def find_emb(path):
+    emb_coord = embedding(path)
+    cv2.namedWindow("frame")
+    cv2.createTrackbar("test", "frame", 85, 100, nothing)
+
+    prev_test = 0
+    while True:
+        test = cv2.getTrackbarPos("test", "frame")
+
+        if test != prev_test:
+            thr = float(test) * 0.01
+            frame = corner_detect(path, emb_coord, thr)[0][0]
+
+            points_corners = [corner_detect(path, emb_coord, thr)[x][1] for x in range(4)]
+
+            bricks = brick_lister(points_corners)
+            brick_attr = []
+            for brick in bricks:
+                brick_attr.append(brick_draw(frame, brick))
+
+            print(len(brick_attr))
+            for attr in brick_attr:
+                print(attr)
+
+            prev_test = test
+
+        cv2.imshow("frame", frame)
+
+        key = cv2.waitKey(1)
+        if key == 27:  # this is the "esc" key
+            break
+        elif key == 32:  # spacebar to save image
+            with open("pickle/bricks.pkl", "wb") as f:
+                f.write(pickle.dumps(bricks, True))
+        
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return brick_attr
+
+def emb_val(path, brick_attr):
+
+    im = cv2.imread(path)
+    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if 2000 < area < 25000:
+            ((x, y), r) = cv2.minEnclosingCircle(contour)
+
+            center_coord = (int(x), int(y))
+            try:
+                for attr in brick_attr:
+                    # print(attr["center_point"])
+                    # print(center_coord)
+                    if distance(attr["center_point"], center_coord) < 150:  # there is an issue with this parameter
+                        print("match_found")
+                        cv2.putText(imgray, ("area = " + str(int(area))), (int(x) - 50, int(y)), cv2.FONT_HERSHEY_SIMPLEX,
+                                    0.6, (127, 127, 127), 2)
+                        error = int(1000 * (1 - (area / attr["area"])))
+                        cv2.putText(imgray, "error = " + (str(error / 10)) + "%", (int(x) - 50, int(y) + 20),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (127, 127, 127), 2)
+            except:
+                print("coordinate error")
+                pass
+
+
+    imgray = cv2.cvtColor(imgray, cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(imgray, contours, -1, (0, 255, 0), 2)
+
+    while True:
+        cv2.imshow('frame', imgray)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
